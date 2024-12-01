9a2fb4311003ffbf2980d793039db9b88c075085
JA <ja@JAdeMacBook-Pro.local>
2024-12-01 13:00:49 +0800

    fixbuild

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -8,6 +8,31 @@ const nextConfig = {
       },
     ],
   },
+  webpack: (config, { isServer }) => {
+    // 启用 Source Map 支持
+    // if (!isServer) {
+    //     config.devtool = 'source-map';
+    // }
+
+    // 确保 Webpack 不解析 `.d.ts` 文件
+    config.module.rules.push({
+      test: /\.d\.ts$/,
+      use: 'null-loader',
+    });
+    // // 忽略 .map 文件的解析错误
+    // config.module.rules.push({
+    //     test: /\.map$/,
+    //     use: ['source-map-loader'],
+    //     enforce: 'pre',
+    // });
+    // 忽略 `.map` 文件
+    config.module.rules.push({
+      test: /\.map$/,
+      use: 'null-loader',
+    });
+
+    return config;
+  }
 };
 
 export default nextConfig;
